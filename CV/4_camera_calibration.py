8cd7d7f59de4f5623a83fe9bcc3267d8ebd5f2e7
km-x <81606491+km-x@users.noreply.github.com>
2023-10-19 13:48:27 +1000

    Update 4_camera_calibration.py
    camera calibration updates by Miguel

--- CV/4_camera_calibration.py
+++ CV/4_camera_calibration.py
@@ -4,42 +4,47 @@ import glob
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-objp = np.zeros((9 * 6, 3), np.float32)
-objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+objp = np.zeros((8 * 5, 3), np.float32)
+objp[:, :2] = np.mgrid[0:8, 0:5].T.reshape(-1, 2)
 
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
 
 
-cap = cv.VideoCapture(1)
+cap = cv.VideoCapture(0)
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
+gray = None
 while True:
     ret, img = cap.read()
     
     if not ret:
         break
+    cv.imshow('img', img)
+    cv.waitKey(1)
 
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, corners = cv.findChessboardCorners(gray, (9, 6), None)
+    ret, corners = cv.findChessboardCorners(gray, (8, 5), None)
 
     if ret:
-        objpoints.append(objp)
-        imgpoints.append(corners)
+        c = cv.waitKey(1)
+        if c == ord(' '):
+            objpoints.append(objp)
+            imgpoints.append(corners)
 
-        corners_draw = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        cv.drawChessboardCorners(img, (9, 6), corners_draw, ret)
+        cv.drawChessboardCorners(img, (8, 5), corners_draw, ret)
+        cv.imshow('corners', img)
+        cv.waitKey(1)
 
-    cv.imshow('img', img)
     c = cv.waitKey(1)
     if c == ord('q'):
         break
-
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+if gray is not None:
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 np.save("ret.npy", ret)
 np.save("mtx.npy", mtx)
@@ -66,3 +71,5 @@ while True:
 
 
 cv.destroyAllWindows()
+
+
